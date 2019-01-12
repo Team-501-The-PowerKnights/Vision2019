93089d80df007cfefe64c184fb567d38c56835c8
ionigman-501 <tf$6a3n8H4lf3lv3n>
2019-01-12 14:23:28 -0500

    remove unneeded calibration items, document

--- util/config.py
+++ util/config.py
@@ -37,54 +37,54 @@ def runConfig(cfg):
       search = config['search']['search']
    except:
       print("ERROR: config.ini does not contain correct parameters. see ./config.correct ")
-      sys.exit(1)
+      # sys.exit(1)
 
    if not os:
       print("INFO: os configuration not present")
+      os = "Linux"
    if not camera:
-      print("ERROR: camera configuration not present")
+      print("ERROR: camera configuration not present.")
       die=1
    sacrificial = None
    try:
-      sacrificial = red_upper[2]
-      sacrificial = red_lower[2]
-      sacrificial = blue_upper[2]
-      sacrificial = blue_lower[2]
       sacrificial = green_upper[2]
       sacrificial = green_lower[2]
    except IndexError:
-      print("ERROR: calibration configuration incomplete")
+      print("ERROR: calibration configuration incomplete.")
       die = 1
    if not sacrificial:
       die = 1
    if not freqFrameNT:
       print("INFO: framerate not specified, using default of 10.")
       freqFrameNT = 10
-   if not vertx:
-      print("Vertices incomplete")
-      die = 1
-   if not verty:
-      print("Vertices incomplete")
-      die = 1
+   if not debug:
+      print("INFO: debug not specified, not debugging.")
+   if not search:
+      print("INFO: search not specified, searching for targets.")
+
+
+
+
    if die > 0:
       print("FATAL ERROR: unable to load vision configuration. Exiting.")
       sys.exit(1)
+
    if debug == '1':
       debug = True
    else:
       debug = False
-   if search == '1':
-      search = True
-   else:
+   if search == '0':
       search = False
-   if debug:
-      print("INFO: debug set.")
-   red = {"red_upper": red_upper, "red_lower": red_lower}
-   blue = {"blue_upper": blue_upper, "blue_lower": blue_lower}
-   green = {"green_upper": green_upper, "green_lower": green_lower}
-   calibration = {"red": red, "blue": blue, "green": green, "debug": debug, "search": search}
+   else:
+      search = True
 
-   return os, camera, calibration, freqFrameNT, vertx, verty
+   if debug:
+      print('INFO: debug set.')
+
+   green = {'green_upper': green_upper, 'green_lower': green_lower}
+   calibration = {'green: green}
+
+   return os, camera, calibration, freqFrameNT, debug, search
 
 
 def write_cal(cal):
