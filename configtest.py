bbcaaafcb56582d7335d27b494cd1a61607ce1bf
ionigman-501 <tf$6a3n8H4lf3lv3n>
2019-01-13 10:57:16 -0500

    code style

--- configtest.py
+++ configtest.py
@@ -1,9 +1,9 @@
 from util.stopwatch import stopwatch
-from util.config import runConfig
+from util.config import run_config
 
 sw=stopwatch('sw')
 sw.start()
-config = runConfig(None)
+config = run_config(None)
 
 print(config)
 timer = sw.get()
