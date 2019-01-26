998842c196b87697a1fde255c341cde9d465a9e3
ionigman-501 <ionigman@gmail.com>
2019-01-26 14:11:34 -0500

    work through some more examples and get a frame timer added.

--- Practice/filtering.py
+++ Practice/filtering.py
@@ -1,39 +1,44 @@
 # Only works with mac currently see comment on line
 import cv2
 import numpy as np
-import sys
-from datetime import datetime
+from util.stopwatch import stopwatch as SW
 
 cap = cv2.VideoCapture(0)  # Need to change the 0 to what ever the camera devices name is
+timer = SW('timer')
+
 
 while cap.isOpened:
+    timer.start()
     ret, frame = cap.read()
     if ret:
         print('Got a frame!! Yay!')
-        frame = cv2.cvtColor(frame,cv2.COLOR_RGB2HSV)
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
         frame = cv2.UMat(frame)
 
-#    hsvup = np.array([183, 204, 139])
-#    hsvlow = np.array([165, 75, 46])
 
-    hsvup = np.array([255, 255, 255])
-    hsvlow = np.array([165, 75, 46])
-
+    hsvup = np.array([186, 209, 142])
+    hsvlow = np.array([162, 70, 41])
     mask = cv2.inRange(frame, hsvlow, hsvup)
-
     cv2.imshow('mask', mask)
 
-
-
-
-
-
-
-
-
-
-
+    kernel = np.ones((5, 5), np.uint8)
+    eroded = cv2.erode(mask,kernel,iterations=4)
+    cv2.imshow('eroded', eroded)
+    dilated = cv2.dilate(eroded,kernel,iterations=4)
+    cv2.imshow('dilated', dilated)
+    fps = 1.0 / timer.get()
+    print('frames per second: %.3f' % fps)
+    cv2.putText(frame, str(fps), (20,20), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(255,255,255), thickness=1, lineType=2)
     cv2.imshow('raw video', frame)
+
+
+
+
+
+
+
+
+
     k = cv2.waitKey(15) & 0xFF
     if k == 27 or k == 113:
         break
