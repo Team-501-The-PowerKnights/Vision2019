dc22ae575044bf4672e30a294298be2402970a9a
ionigman-501 <ionigman@gmail.com>
2019-02-10 14:04:47 -0500

    doc changes and cleanup

--- camera_init.sh
+++ camera_init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# init video stream at 60fps at 320x240
+# init video stream
 mjpg_streamer -o "output_http.so -w ./www -p 1180" -i "input_uvc.so -d /dev/video0 -f 30 -r 640x480 -n" &
 
 # turns off auto-exposure
@@ -12,3 +12,4 @@ v4l2-ctl --set-ctrl exposure_absolute=15
 v4l2-ctl --set-ctrl white_balance_temperature_auto=0
 v4l2-ctl --set-ctrl white_balance_temperature=5000
 echo "done"
+
